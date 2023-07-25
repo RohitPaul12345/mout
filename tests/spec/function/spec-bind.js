5208e7b396f1bdc55780861f003b66de612b001a
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-07-25 21:51:34 +0530

    Update spec-bind.js

--- tests/spec/function/spec-bind.js
+++ tests/spec/function/spec-bind.js
@@ -2,15 +2,15 @@ define(['mout/function/bind'], function (bind) {
 
     describe('function/bind()', function(){
 
-        var o1 = {val : 'bar'};
-        var o2 = {val : 123};
+        let o1 = {val : 'bar'};
+        let o2 = {val : 123};
 
         function getVal(){
             return this.val;
         }
 
         function doIt(a, b, c){
-            var str = '';
+            let str = '';
             str += a? a : '';
             str += b? b : '';
             str += c? c : '';
@@ -19,15 +19,15 @@ define(['mout/function/bind'], function (bind) {
 
 
         it('should change execution context', function(){
-            var a = bind(getVal, o1);
-            var b = bind(getVal, o2);
+            let a = bind(getVal, o1);
+            let b = bind(getVal, o2);
             expect( a() ).toEqual('bar');
             expect( b() ).toEqual(123);
         });
 
         it('should curry args', function(){
-            var a = bind(doIt, o1, ' a', 'b', 'c');
-            var b = bind(doIt, o2, '456');
+            let a = bind(doIt, o1, ' a', 'b', 'c');
+            let b = bind(doIt, o2, '456');
             expect( a() ).toEqual('bar abc');
             expect( b() ).toEqual('123456');
         });
